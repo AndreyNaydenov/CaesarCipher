d681b50a5341159507bd665739d2daf500cd22e6
Naydenov <donute@bk.ru>
2018-01-13 22:21:51 +0600

    design refactored

--- src/edu/naydenov/Main.java
+++ src/edu/naydenov/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("MainView.fxml"));
-        primaryStage.setTitle("Шифр Цезаря");
+        primaryStage.setTitle("Caesar cipher");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
